b92bdd6b6a7988e3805a7af57b1f80e0b15848f3
Jovana Kovacevic <jovana1005@gmail.com>
2021-11-28 19:44:51 +0100

    added random user reviews js file

---
+++ js/random_user_reviews.js
@@ -0,0 +1,27 @@
+$(() => $.ajax({
+    url: 'https://randomuser.me/api/?results=4',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+      
+      $.each(data.results, function(index, person) {
+        
+        let number = Math.floor(Math.random() * (6 - 1) + 1)
+        let stars = ""
+        for (i = 0; i <= number; i++) {
+          stars += <i class="fas fa-star stars"></i>
+        }
+
+        const reviewHtml =
+          `<div class="reviews">
+            <img class="random-person-image" src="${person.picture.medium}" alt=""></img>
+            <h1>${`${person.name.first} ${person.name.last}`}</h1>
+            <span>${stars}</span>
+          </div>`
+
+        $('#reviews-div').append(reviewHtml)
+      })
+    }
+  })
+
+)
