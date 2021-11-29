5f6c8a606bb5305d6b66bef044fe6abcda545998
Jovana Kovacevic <jovana1005@gmail.com>
2021-11-29 13:02:23 +0100

    added comments to random user reviews

--- js/random_user_reviews.js
+++ js/random_user_reviews.js
@@ -1,25 +1,32 @@
 $(() => $.ajax({
     url: 'https://randomuser.me/api/?results=4',
     dataType: 'json',
-    success: function(data) {
-      console.log(data);
-      
-      $.each(data.results, function(index, person) {
-        
-        let number = Math.floor(Math.random() * (6 - 1) + 1)
-        let stars = ""
-        for (i = 0; i <= number; i++) {
-          stars += <i class="fas fa-star stars"></i>
+    success: function(userData) {
+
+      $.ajax({
+        url: 'https://opensheet.vercel.app/1gDEZwxw64n-jEWVBSXneFmAubQ7nVZ9rDbTDpSZVrAw/Sheet1',
+        dataType: 'json',
+        success: function(comments) {
+          
+          userData.results.forEach(function(person, index) {
+            let number = Math.floor(Math.random() * (6 - 4) + 4)
+            let stars = ""
+            for (i = 0; i < number; i++) {
+              stars += '<i class="fas fa-star stars"></i>'
+            }            
+    
+            const reviewHtml =
+              `<div class="reviews">
+                <img class="random-person-image" src="${person.picture.large}" alt=""></img>
+                <h1 class="person-name">${`${person.name.first} ${person.name.last}`}</h1>
+                <p>${comments[index].Review}</p>
+                <p>${stars}</p>
+              </div>`
+    
+            $("#reviews-div").append(reviewHtml)
+          })
+
         }
-
-        const reviewHtml =
-          `<div class="reviews">
-            <img class="random-person-image" src="${person.picture.medium}" alt=""></img>
-            <h1>${`${person.name.first} ${person.name.last}`}</h1>
-            <p>${stars}</p>
-          </div>`
-
-        $("#reviews-div").append(reviewHtml)
       })
     }
   })
