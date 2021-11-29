5a0656fdbb671e85359f706a60861fc259546daa
Jovana Kovacevic <jovana1005@gmail.com>
2021-11-29 14:18:57 +0100

    modified the order of content in the reviews

--- js/random_user_reviews.js
+++ js/random_user_reviews.js
@@ -18,9 +18,9 @@ $(() => $.ajax({
             const reviewHtml =
               `<div class="reviews">
                 <img class="random-person-image" src="${person.picture.large}" alt=""></img>
-                <h1 class="person-name">${`${person.name.first} ${person.name.last}`}</h1>
-                <p class="comments">${comments[index].Review}</p>
                 <p>${stars}</p>
+                <p class="comments">${comments[index].Review}</p>
+                <h1 class="person-name">${`-${person.name.first}-`}</h1>
               </div>`
     
             $("#reviews-div").append(reviewHtml)
